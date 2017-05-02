d0f51039bc6694164c1313e0fd5c9ca0b8be3a54
HeyKelven <victork.laerte@gmail.com>
2017-05-01 21:47:34 -0300

    GerarPessoasAleatórias
    Agora cada vetor de char tem a quantidade instanciada igual a de Letras que contém

--- TesteFormulas/TesteFormulas/Pessoas.cs
+++ TesteFormulas/TesteFormulas/Pessoas.cs
@@ -96,38 +96,32 @@ namespace TesteFormulas
             //Recebe a quantidade de usuario, tem que receber o char do ultimo guiche e o ultimo turno que algum "cliente" deve chegar
             //retorna um vetor de pessoas. Que seria a fila do arquivo txt fila.
 
-            int aux;
             Pessoas[] b = new Pessoas[quantidade];
             Random ran = new Random();
-            //aleatorizando um vetor chegada com chegadas de 1 até o ultimotempochegada
-            int[] chegada = new int[quantidade];
-
-            for (int j = 0; j < quantidade; j++) 
-                chegada[j] = ran.Next(1, ultimotempochegada);
-            
-            Array.Sort<int>(chegada);   //ordenando o vetor com as chegadas
-
-
             for (int i = 0; i < quantidade; i++)
             {
                 b[i] = new Pessoas();
-                
                 b[i].usuario = (i + 1);
-                b[i].chegada = chegada[i];  //settando a chegada das pessoas de acordo com o vetor chegada
-
-                b[i].guiches = new char[Convert.ToInt32(ultimoguiche) - (Convert.ToInt32('A') - 1)];
-                Console.WriteLine("tamanho guiches: "+b[i].guiches.Length); //verificando o tamanho de cada array
-                b[i].guiches[0] = 'A';
-                for (int j = 1; j < ran.Next(5, (Convert.ToInt32(ultimoguiche) + 1)); j++)
-                {
-                    aux = b[i].guiches[j - 1];
-                    if (aux == (Convert.ToInt32(ultimoguiche)))
-                        break;
-                    b[i].guiches[j] = ((char)ran.Next((aux + 1), (Convert.ToInt32(ultimoguiche))));
-                }
+                b[i].chegada = ran.Next(1, ultimotempochegada);
+                string novo = "A";
+                 for (int j = 1; j < ran.Next(5, (Convert.ToInt32(ultimoguiche) + 1)); j++)
+               {
+                   if (novo[novo.Length - 1] == ultimoguiche - 1) 
+                   {
+                       if (ran.Next(0, 2) == 1)
+                       {
+                           novo += ultimoguiche;
+                           break;
+                       }
+                       else break;
+                   }
+                   novo += (char)(ran.Next(novo[novo.Length - 1]+1, (Convert.ToInt32(ultimoguiche))));
+               }
+               b[i].guiches = novo.ToCharArray();
             }
             return b;
         }
+        
         public static void resetPessoas(Pessoas[] pessoas) {
             for (int i = 0; i < pessoas.Length; i++)
                 pessoas[i].atualGuiche = 0;
